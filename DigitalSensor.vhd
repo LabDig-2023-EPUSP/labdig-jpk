4470c7334c2c0a655185e95466ab1fca9e182061
KrWanderley <81878703+KrWanderley@users.noreply.github.com>
2023-07-05 14:46:25 -0300

    Adding HMI

--- DigitalSensor.vhd
+++ DigitalSensor.vhd
@@ -163,3 +163,209 @@ architecture Decoder_arc of Decoder is
 	begin
 
 end architecture;
+
+------------------------------
+--HUMAN-MACHINE INTERFACE
+------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity BinaryToDecimal is
+    port (
+        binary_input : in std_logic_vector(15 downto 0);
+        decimal_output : out integer
+    );
+end entity BinaryToDecimal;
+
+architecture Behavioral of BinaryToDecimal is
+begin
+    process(binary_input)
+    	begin
+            decimal_output <= to_integer(signed(binary_input(12 downto 4)));	
+        end process;
+end architecture Behavioral;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity HMI is
+	port (
+		wordReady: in bit;
+    	binary_input : in std_logic_vector(15 downto 0);
+		control: in std_logic_vector(2 downto 0);
+		tempMin: in integer;
+		tempMax: in integer;
+		scale: in integer;
+      display_output : out std_logic_vector(41 downto 0)
+    );
+end entity HMI;
+
+architecture HMI_arch of HMI is
+
+    component BinaryToDecimal
+        port (
+            binary_input : in std_logic_vector(15 downto 0);
+            decimal_output : out integer
+        );
+    end component;
+	 
+	 component mux8x1 is
+		port(
+			sel: in std_logic_vector(2 downto 0);
+			I0, I1, I2, I3, I4, I5, I6, I7: in std_logic_vector(41 downto 0);
+			Y: out std_logic_vector(41 downto 0)
+		);
+	end component mux8x1;
+	 
+    signal decimal, centena, dezena, unidade, decimal_s: integer;
+	 signal min_dezena, min_unidade, max_dezena, max_unidade, min, max: integer;
+	 signal display_0, display_1, display_2, display_3, display_4, display_5, display_6, display_7: std_logic_vector(41 downto 0);
+
+begin
+    
+	BTD: BinaryToDecimal port map(binary_input, decimal);
+		
+	MX: Mux8x1 port map(control, display_0, display_1, display_2, display_3, display_4, display_5, display_6, display_7, display_output);
+			
+	min <= tempMin;
+	max <= tempMax;
+	
+	min_unidade <= min mod 10;
+	min_dezena <= min/10;
+	max_unidade <= max mod 10;
+	max_dezena <= max/10;
+	
+	display: process(binary_input, scale)
+    	begin
+			if (scale = 2) then
+				decimal_s <= decimal + 273;
+			elsif (scale = 1) then
+				decimal_s <= decimal + decimal + 32;
+			else
+				decimal_s <= decimal;
+			end if;
+        end process;
+		  
+	 unidade <= decimal_s mod 10;
+    dezena <= (decimal_s mod 100)/10;
+    centena <= (decimal_s mod 1000)/100;
+	
+	--Display 0
+	display_0(41 downto 35) <= "0000111";
+	display_0(34 downto 28) <= "0110111";
+	
+	with centena select display_0(27 downto 21) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others;
+	
+	with dezena select display_0(20 downto 14) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others;
+	
+	with unidade select display_0(13 downto 7) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others; 
+
+	 with scale select display_0(6 downto 0) <= "1000110" when 0, 
+																"0001110" when 1,
+																"0001010" when 2,
+																"1000110" when others;
+	--Displays 1,2 e 3
+	display_1 <= "111111111111111111111111111110111111000110";
+	display_2 <= "111111111111111111111111111110111110001110";
+	display_3 <= "111111111111111111111111111110111110001010";
+	
+	 --Display 4
+    
+	 display_4(41 downto 35) <= "1101010";
+	 display_4(34 downto 28) <= "1001000";
+	 display_4(27 downto 21) <= "0110111";
+	 
+    with min_dezena select display_4(20 downto 14) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others;
+    
+	 with min_unidade select display_4(13 downto 7) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others;
+   
+	 display_4(6 downto 0) <= "1000110";
+	
+	 --Display 5
+	 display_5(41 downto 35) <= "1101010";
+	 display_5(34 downto 28) <= "0001001";
+	 display_5(27 downto 21) <= "0110111";
+	 
+    with max_dezena select display_5(20 downto 14) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others;
+        
+    with max_unidade select display_5(13 downto 7) <=
+    	  "1000000" when 0,
+        "1111001" when 1,
+        "0100100" when 2,
+        "0110000" when 3,
+        "0011001" when 4,
+        "0010010" when 5,
+        "0000010" when 6,
+        "1111000" when 7,
+        "0000000" when 8,
+        "0010000" when others;
+	 
+	 display_5(6 downto 0) <= "1000110";
+	 
+	 --Displays 6 e 7
+	 display_6 <= "100011100001100100001111111111111111111111";
+	 display_7 <= "000001110000010100100010010000001101001100";
+	
+end architecture HMI_arch;
